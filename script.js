8617d3dbcad4a03fe8dd02eda7c61d8ad17465dd
Jyoti07-git <144542779+Jyoti07-git@users.noreply.github.com>
2023-11-14 23:29:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+function submitComment(postNumber) {
+    const name = document.getElementById(`name${postNumber}`).value;
+    const comment = document.getElementById(`comment${postNumber}`).value;
+  
+    const commentContainer = document.getElementById(`
+  
